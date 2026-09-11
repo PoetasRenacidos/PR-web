dab81201da38a50baa0cd92edfab81b0224545b6
PinillosDev <pinillosdev@gmail.com>
2026-09-10 21:12:34 -0500

    DDBB models created for users

--- pr38_web/users/models.py
+++ pr38_web/users/models.py
@@ -1,7 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from django.db import models
 
-# Las 8 artes clásicas/contemporáneas para el perfil del usuario
-# Campo de tipo CHOICE para que el usuario seleccione una opción.
+# Las 8 artes clásicas/contemporáneas para el perfil del usuario/tallerista
+# Campo de tipo CHOICE para que el usuario seleccione una única opción.
 ARTES_CHOICES = [
     ('arquitectura',    'Arquitectura'),
     ('escultura',       'Escultura'),
@@ -13,10 +16,31 @@ ARTES_CHOICES = [
     ('cine',            'Cine / Audiovisual'),
 ]
 
-class normalUser(models.Model):
+class NormalUser(models.Model):
     name = models.CharField(max_length=120, verbose_name="Nombre completo")
-    email = models.EmailField(unique=True, verbose_name="Correo electrónico")
-    phone_number = models.CharField(blank=True, null=True, max_length=20, verbose_name="Teléfono")
+    email = models.EmailField(unique=True, db_index=True, verbose_name="Correo electrónico")
+    phone_number = models.BigIntegerField(blank=True, null=True, max_length=20, verbose_name="Teléfono")
+    password = models.CharField(max_length=128, default="0000", verbose_name="Contraseña")
+    fav_art = models.CharField(max_length=30, choices=ARTES_CHOICES, verbose_name="Arte favorito")
+    city = models.CharField(blank=True, null=True, max_length=50, verbose_name="Ciudad de residencia")
+
+class TeacherUser(models.Model):
+    name = models.CharField(max_length=120, verbose_name="Nombre completo")
+    email = models.EmailField(unique=True, db_index=True, verbose_name="Correo electrónico")
+    phone_number = models.BigIntegerField(blank=True, null=True, max_length=20, verbose_name="Teléfono")
     password = models.CharField(max_length=128, default="", verbose_name="Contraseña")
-    fav_art = models.CharField(max_length=30, choices=ARTES_CHOICES, verbose_name="Arte favirito")
-    city = models.CharField(blank=True, null=True, max_length=50, verbose_name="Ciudad residencia")
+    profession = models.CharField(max_length=60, choices=ARTES_CHOICES, verbose_name="Rubro/Profesión")
+    years_experience = models.IntegerField(verbose_name="Años de experiencia")
+    city = models.CharField(blank=True, null=True, max_length=50, verbose_name="Ciudad de residencia")
+
+class user_per_teacher(models.Model): # Intermediate table to mantain DDBB standards
+    fk_NormalUser = models.ForeignKey(NormalUser, on_delete=models.CASCADE, verbose_name="Usuario normal")
+    fk_TeacherUser = models.ForeignKey(TeacherUser, on_delete=models.CASCADE, verbose_name="Profesor")
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint( # Avoid duplicate pairs between fk_NormalUser & fk_TeacherUser
+                fields=["fk_NormalUser", "fk_TeacherUser"],
+                name="unique_user_teacher"
+            )
+        ]
