c919ffc0b4f1996e8a87fd24176dd28f32d78080
PinillosDev <pinillosdev@gmail.com>
2026-09-17 15:27:45 -0500

    modelos de los usuarios adelantados, quedando faltando las relaciones con las entidades ProductoVenta, Post y Obras

--- pr38_web/users/models.py
+++ pr38_web/users/models.py
@@ -16,15 +16,15 @@ ARTES_CHOICES = [
     ('cine',            'Cine / Audiovisual'),
 ]
 
-class NormalUser(models.Model):
+class UsuarioBase(models.Model):
     name = models.CharField(max_length=120, verbose_name="Nombre completo")
     email = models.EmailField(unique=True, db_index=True, verbose_name="Correo electrónico")
     phone_number = models.BigIntegerField(blank=True, null=True, max_length=20, verbose_name="Teléfono")
-    password = models.CharField(max_length=128, default="0000", verbose_name="Contraseña")
+    password = models.CharField(max_length=128, verbose_name="Contraseña")
     fav_art = models.CharField(max_length=30, choices=ARTES_CHOICES, verbose_name="Arte favorito")
     city = models.CharField(blank=True, null=True, max_length=50, verbose_name="Ciudad de residencia")
 
-class TeacherUser(models.Model):
+class UsuarioTallerista(models.Model):
     name = models.CharField(max_length=120, verbose_name="Nombre completo")
     email = models.EmailField(unique=True, db_index=True, verbose_name="Correo electrónico")
     phone_number = models.BigIntegerField(blank=True, null=True, max_length=20, verbose_name="Teléfono")
@@ -32,15 +32,4 @@ class TeacherUser(models.Model):
     profession = models.CharField(max_length=60, choices=ARTES_CHOICES, verbose_name="Rubro/Profesión")
     years_experience = models.IntegerField(verbose_name="Años de experiencia")
     city = models.CharField(blank=True, null=True, max_length=50, verbose_name="Ciudad de residencia")
-
-class user_per_teacher(models.Model): # Intermediate table to mantain DDBB standards
-    fk_NormalUser = models.ForeignKey(NormalUser, on_delete=models.CASCADE, verbose_name="Usuario normal")
-    fk_TeacherUser = models.ForeignKey(TeacherUser, on_delete=models.CASCADE, verbose_name="Profesor")
-
-    class Meta:
-        constraints = [
-            models.UniqueConstraint( # Avoid duplicate pairs between fk_NormalUser & fk_TeacherUser
-                fields=["fk_NormalUser", "fk_TeacherUser"],
-                name="unique_user_teacher"
-            )
-        ]
+    is_staff = models.BooleanField(default=False)
