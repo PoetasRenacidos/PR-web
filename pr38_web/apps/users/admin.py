2f878b1e5a3aa7e6b576dba83f913df793e7e6d4
PinillosDev <pinillosdev@gmail.com>
2026-09-18 12:09:19 -0500

    Cifrado de contraseña EXITOSO

--- pr38_web/apps/users/admin.py
+++ pr38_web/apps/users/admin.py
@@ -3,23 +3,6 @@
 
 from django.contrib import admin
 from apps.users.models import UsuarioTallerista, UsuarioBase
-from django.forms import PasswordInput
 
 admin.site.register(UsuarioBase)
 admin.site.register(UsuarioTallerista)
-
-
-class UsuarioBaseAdmin(admin.ModelAdmin):
-    def get_form(self, request, obj=None, **kwargs):
-        form = super().get_form(request, obj, **kwargs)
-        if 'password' in form.base_fields:
-            form.base_fields['password'].widget = PasswordInput(render_value=False)
-        return form
-
-
-class UsuarioTallerista(admin.ModelAdmin):
-    def get_form(self, request, obj=None, **kwargs):
-        form = super().get_form(request, obj, **kwargs)
-        if 'password' in form.base_fields:
-            form.base_fields['password'].widget = PasswordInput(render_value=False)
-        return form
